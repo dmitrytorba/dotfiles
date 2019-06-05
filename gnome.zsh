8791cd7751faa664c416ffebba4f38128c7d4db1
Dmitry Torba <dmitrytorba@gmail.com>
2019-06-05 10:43:06 -0700

    p52s

---
+++ gnome.zsh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier', 'ctrl:swap_lalt_lctl']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Primary>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Primary><Shift>Tab']"
+
+
+
