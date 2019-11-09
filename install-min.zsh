f2f7ffee3e9632218363adb13fd09e13d2752e33
Dmitry Torba <dmitrytorba@gmail.com>
2019-11-09 05:46:14 -0800

    minimal installer

---
+++ install-min.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+cd ~
+ln -s dotfiles/.tmux.conf .
+ln -s dotfiles/.emacs .
+ln -s dotfiles/.zshrc .
+git config --global user.email 'dmitrytorba@gmail.com'
+git config --global user.name 'Dmitry Torba'
