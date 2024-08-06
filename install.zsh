ba924363c30d0132282a9df89f38e66c85de6e0d
Dmitry Torba <dmitrytorba@gmail.com>
2024-08-06 09:59:29 -0700

    feat(install) add starship

--- install.zsh
+++ install.zsh
@@ -7,4 +7,5 @@ ln -s dotfiles/.zshrc .
 git config --global user.email 'dmitrytorba@gmail.com'
 git config --global user.name 'Dmitry Torba'
 
+curl -sS https://starship.rs/install.sh | sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
